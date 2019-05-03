f83965df0dbb584d0b8dd4441df4f72baadf299a
João Azevedo <1111476@isep.ipp.pt>
2019-05-03 19:26:31 +0100

    Updated Serial tx script

--- dev/serial-app/Tx/serial-app_tx.py
+++ dev/serial-app/Tx/serial-app_tx.py
@@ -4,7 +4,7 @@ import serial
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
 	port='/dev/ttyUSB0',
-	baudrate=9600,
+	baudrate=115200,
 	parity=serial.PARITY_NONE,
 	stopbits=serial.STOPBITS_ONE,
 	bytesize=serial.EIGHTBITS
@@ -35,9 +35,7 @@ while 1 :
 		# let's wait one second before reading output (let's give device time to answer)
 		time.sleep(1)
 		while ser.inWaiting() > 0:
-			out += ser.read(1)
-			if out != '':
-				print (">>" + out)
+			print("inWaiting")
 
 
 
--- dev/serial-app/Tx/serial_cli.py
+++ dev/serial-app/Tx/serial_cli.py
@@ -9,11 +9,12 @@ import serial
 
 choices = {'c':'c', 'a':'a'}
 send_array = []
+send_string = ""
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
 	port='/dev/ttyUSB0',
-	baudrate=9600,
+	baudrate=115200,
 	parity=serial.PARITY_NONE,
 	stopbits=serial.STOPBITS_ONE,
 	bytesize=serial.EIGHTBITS
@@ -26,16 +27,24 @@ ser.isOpen()
 
 
 def menu_entry(option):
+	global send_string
 	if option == 'c':
 		n_uavs = int(input("Drones of network: "))
 		for x in range(0, n_uavs):
 			command = input("Introduce a command for drone " + str(x+1) + ": ")
 			uav_no = x+1
-			uav_array = [ uav_no, command ]
-			send_array.append(uav_array)
+			send_string += str(uav_no) + "_" + command + "_"
+			print("Cycle: %d\n UAV nº: %d\n String to send: %s\n",x, uav_no, send_string)
+			#uav_array = [ uav_no, command ]
+			#send_array.append(uav_array)
 
-		str_to_send = str(send_array)+"\r\n"
+		#str_to_send = str(n_uavs)+"|"+str(send_array)+"\r\n"
+		str_to_send = str(n_uavs)+"|"+send_string+"\r\n"
+		print(str_to_send)
+		#print("Sending info")
 		ser.write(str_to_send.encode())
+		#print("message sent")
+		#print("sleep 1s")
 		time.sleep(1)
 	elif option == 'a':
 		print("A")
@@ -51,8 +60,10 @@ def menu_show():
 
 while 1 :
 	menu_show()
-	old_array = send_array
-	send_array = []
+	old_string = send_string
+	send_string = ""
+	#old_array = send_array
+	#send_array = []
 
 
 
